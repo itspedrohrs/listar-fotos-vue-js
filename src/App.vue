b20865f11d4d7adadff679dceab2490d39c7db1e
itspedrohrs <itspedrohrs@gmail.com>
2021-08-31 21:59:01 -0300

    feat: :tada: commit initial

---
+++ src/App.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+        <h1>{{titulo}}</h1>
+    <ul>
+      <li v-for="foto of fotos">
+        <img :src="foto.url" :alt="foto.titulo" :title="foto.titulo" />
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+
+  data(){
+    return {
+      titulo : 'Project Vue',
+      fotos: []
+    }
+  },
+  created() {
+    
+    this.$http.get('http://localhost:3000/v1/fotos')
+    .then(res => res.json())
+    .then(fotos => this.fotos = fotos, err => console.log(err))
+
+  }
+
+};
+</script>
+
