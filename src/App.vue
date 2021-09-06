d91eaf276865d43e1a34114b8054544139af8c72
itspedrohrs <itspedrohrs@gmail.com>
2021-09-05 21:40:42 -0300

    feat: :sparkles: Add filter to search at photos list

--- src/App.vue
+++ src/App.vue
@@ -1,31 +1,85 @@
 <template>
-  <div>
-        <h1>{{titulo}}</h1>
-    <ul>
-      <li v-for="foto of fotos">
-        <img :src="foto.url" :alt="foto.titulo" :title="foto.titulo" />
+  <div class="corpo">
+    <h1 class="centralizado">{{ titulo }}</h1>
+
+    <input type="search" class="fitro" v-on:input="filtro = $event.target.value" placeholder="Filtro pelo titulo">
+    <ul class="lista-fotos">
+      <li v-for="foto of fotosComFiltro" class="lista-fotos-item">
+
+        <meu-painel :titulo="foto.titulo">
+          <img class="imagem-responsiva" :src="foto.url" :alt="foto.titulo" :title="foto.titulo"/>
+        </meu-painel>
+
       </li>
     </ul>
   </div>
 </template>
 
 <script>
-export default {
+import Painel from "./components/shared/painel/Painel";
 
-  data(){
+export default {
+  components: {
+    'meu-painel': Painel
+  },
+  data() {
     return {
-      titulo : 'Project Vue',
-      fotos: []
+      titulo: 'Project Vue',
+      fotos: [],
+      filtro: ''
     }
   },
+
+  computed: {
+    fotosComFiltro() {
+      if (this.filtro) {
+        // Filtrar
+        return this.fotos
+      } else {
+        let exp = new RegExp(this.filtro.trim(), 'i')
+        return this.fotos.filter(foto => exp.test(foto.titulo))
+      }
+    }
+  },
+
   created() {
-    
+
     this.$http.get('http://localhost:3000/v1/fotos')
-    .then(res => res.json())
-    .then(fotos => this.fotos = fotos, err => console.log(err))
+        .then(res => res.json())
+        .then(fotos => this.fotos = fotos, err => console.log(err)
+        )
 
   }
 
 };
 </script>
 
+<style>
+.centralizado {
+  text-align: center;
+}
+
+.corpo {
+  font-family: Helvetica, sans-serif;
+  margin: 0 auto;
+  width: 96%;
+}
+
+.lista-fotos {
+  list-style: none;
+}
+
+.lista-fotos .lista-fotos-item {
+  display: inline-block;
+}
+
+.imagem-responsiva {
+  width: 100%;
+
+}
+
+.fitro {
+  display: block;
+  width: 100%;
+}
+</style>
---
+++ src/components/shared/painel/Painel.vue
@@ -0,0 +1,44 @@
+<template>
+        <div class="painel">
+          <h2 class="painel-titulo">{{titulo}}</h2>
+          <slot class="paine-conteudo">
+          </slot>
+        </div>
+</template>
+
+<script>
+export default {
+  props: [
+      'titulo'
+  ]
+};
+</script>
+
+<style scoped>
+/* estilo do painel */
+
+.painel {
+  padding: 0 auto;
+  border: solid 2px grey;
+  display: inline-block;
+  margin: 5px;
+  box-shadow: 5px 5px 10px grey;
+  width: 200px;
+  height: 100%;
+  vertical-align: top;
+  text-align: center;
+}
+
+.painel .painel-titulo {
+  text-align: center;
+  border: solid 2px;
+  background: lightblue;
+  margin: 0 0 15px 0;
+  padding: 10px;
+  text-transform: uppercase;
+}
+
+* {
+  box-shadow: 5px 5px 5px black;
+}
+</style>
