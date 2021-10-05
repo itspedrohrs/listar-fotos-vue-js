50335fd5275330d33bfddde0609dd2c7c52cc55e
itspedrohrs <itspedrohrs@gmail.com>
2021-10-04 22:48:13 -0300

    :sparkles: Create component image responsive

---
+++ src/components/shared/image-responsive/ImageResponsive.vue
@@ -0,0 +1,15 @@
+<template>
+  <img class="imagem-responsiva" :src="url" :alt="title" :title="title"/>
+</template>
+
+<script>
+export default {
+  props: ['url', 'title']
+}
+</script>
+
+<style scoped>
+.imagem-responsiva {
+  width: 100%;
+}
+</style>
