b4d133f30b84602c68921cc53ba0fd1c8af9fa7c
itspedrohrs <itspedrohrs@gmail.com>
2021-10-10 19:33:44 -0300

    :sparkles: add option menu in application

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,30 @@
 <template>
   <div class="corpo">
-    <router-view></router-view>
+
+    <nav>
+      <ul>
+        <li v-for="route in routes">
+          <router-link :to="route.path ? route.path :  '/'  ">{{ route.title }}</router-link>
+        </li>
+      </ul>
+    </nav>
+    <transition name="pagina">
+      <router-view></router-view>
+    </transition>
   </div>
 </template>
 
 <script>
 
+import {routes} from './routes'
+
 export default {
+
+  data() {
+    return {
+      routes
+    }
+  }
 
 }
 </script>
@@ -17,5 +35,13 @@ export default {
   font-family: Helvetica, sans-serif;
   margin: 0 auto;
   width: 96%;
+}
+
+.pagina-enter, .pagina-leave-active {
+  opacity: 0;
+}
+
+.pagina-enter-active, .pagina-leave-active {
+  transition: opacity 1.5s;
 }
 </style>
