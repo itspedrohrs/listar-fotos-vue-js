93ac9d11a9b698627d44d1dd97eec4cf5977f404
itspedrohrs <itspedrohrs@gmail.com>
2021-10-16 23:37:10 -0300

    :sparkles: define types variables in directive

--- src/directives/RotationElement.js
+++ src/directives/RotationElement.js
@@ -5,17 +5,41 @@ Vue.directive('my-tranform', {
     bind(el, binding, vnode) {
         let current = 0
         el.addEventListener('dblclick', () => {
-            let increment = 90, timeTransition = 5, animate = false
+
+            /**
+             *
+             * @type {string} typeAnimate - define type animation to component image
+             */
+
+            let typeAnimate
+
+            /**
+             *
+             * @type {number} timeTransition - define type animation to component image
+             */
+            let timeTransition = 5
+
+            /**
+             *
+             * @type {number} increment - define increment case if typeAnimate = rotation
+             */
+            let increment = 90
 
             if (binding.value) {
-                increment = binding.value.rotation
-                timeTransition = binding.value.timeTransition
-                animate = binding.value.animate
+                if(binding.value.typeAnimate === 'rotation' || binding.value.typeAnimate === ''){
+                    increment = binding.value.rotation
+                    timeTransition = binding.value.timeTransition
+                    current += increment
+                    typeAnimate = `rotate(${current}deg)`
+                } else if (binding.value.typeAnimate === 'scale'){
+                    timeTransition = binding.value.timeTransition
+                    current = '1.2'
+                    typeAnimate = `scale(${current})`
+                }
             }
 
-            current += increment
-            el.style.transform = `rotate(${current}deg)`
-            if(animate) el.style.transition = `transform ${timeTransition}s`
+            el.style.transform = typeAnimate
+            if(binding.value.animate) el.style.transition = `transform ${timeTransition}s`
         })
     }
 
