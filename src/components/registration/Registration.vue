009b2d07e4de9ebe67ac4052e7bd3f8a7ab02185
itspedrohrs <itspedrohrs@gmail.com>
2021-10-17 22:08:29 -0300

    :sparkles: add template registration to new image application

--- src/components/registration/Registration.vue
+++ src/components/registration/Registration.vue
@@ -1,15 +1,75 @@
 <template>
-  <h2 class="center">Cadastrar</h2>
+
+  <div>
+    <h1 class="centralizado">Cadastro</h1>
+    <h2 class="centralizado"></h2>
+
+    <form id="form-registration-image">
+      <div class="controle">
+        <label for="titulo">Título</label>
+        <input id="titulo" autocomplete="off" placeholder="Informe um título para imagem">
+      </div>
+
+      <div class="controle">
+        <label for="url">Url</label>
+        <input id="url" autocomplete="off" placeholder="Informe a url para imagem">
+        <image-responsive url="" title=""/>
+      </div>
+
+      <div class="controle">
+        <label for="descricao">Descrição</label>
+        <textarea id="descricao" autocomplete="off"></textarea>
+      </div>
+
+      <div class="centralizado">
+        <my-button label="Salvar" title="Salvar" type="submit" classButton="default"/>
+        <router-link to="/"><my-button label="Voltar" title="Voltar" type="button" classButton="come-back" /></router-link>
+      </div>
+
+    </form>
+  </div>
 </template>
 
+
 <script>
+
+import ImageResponsive from "../shared/image-responsive/ImageResponsive";
+import Button from "../shared/button/Button";
+
 export default {
 
+  components: {
+    'image-responsive': ImageResponsive,
+    'my-button': Button
+  }
+
 }
+
 </script>
 
 <style scoped>
-.center {
+
+.centralizado {
   text-align: center;
 }
+.controle {
+  font-size: 1.2em;
+  margin-bottom: 20px;
+
+}
+.controle label {
+  display: block;
+  font-weight: bold;
+}
+
+.controle label + input, .controle textarea {
+  width: 100%;
+  font-size: inherit;
+  border-radius: 5px
+}
+
+.centralizado {
+  text-align: center;
+}
+
 </style>
