7bc31562a5cf267c3b2d8a9183623464950f4bc9
itspedrohrs <itspedrohrs@gmail.com>
2021-11-01 21:58:54 -0300

    feat: alter url in create image

--- src/routes.js
+++ src/routes.js
@@ -3,5 +3,5 @@ import Home from './components/home/Home'
 
 export const routes = [
     {path: '', component: Home, title: 'Home'}, // direciona para pagina home
-    {path: '/create', component: RegisterImage, title: 'Cadastro'} // direciona para pagina de cadastro das fotos
+    {path: '/image/create', component: RegisterImage, title: 'Cadastro'} // direciona para pagina de cadastro das fotos
 ]
