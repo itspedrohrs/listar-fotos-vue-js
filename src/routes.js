34042e4a6e3b3e6717791fa9370994c5f0c3b84b
itspedrohrs <itspedrohrs@gmail.com>
2021-12-13 21:27:18 -0300

    feat: add prop menu and name in routes aplicaiton

--- src/routes.js
+++ src/routes.js
@@ -2,6 +2,6 @@ import RegisterImage from './components/registration/Registration'
 import Home from './components/home/Home'
 
 export const routes = [
-    {path: '', component: Home, title: 'Home'}, // direciona para pagina home
-    {path: '/image/create', component: RegisterImage, title: 'Cadastro'} // direciona para pagina de cadastro das fotos
+    {path: '', name: 'home', component: Home, title: 'Home', menu: true}, // direciona para pagina home
+    {path: '/image/create', name:'create_image', component: RegisterImage, title: 'Cadastro', menu: true}, // direciona para pagina de cadastro das fotos
 ]
