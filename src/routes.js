a579eefe4f74327799e0c2c647a41c3c0e1265e7
itspedrohrs <itspedrohrs@gmail.com>
2022-01-23 20:50:44 -0300

    :sparkles: add route to update image

--- src/routes.js
+++ src/routes.js
@@ -4,4 +4,6 @@ import Home from './components/home/Home'
 export const routes = [
     {path: '', name: 'home', component: Home, title: 'Home', menu: true}, // direciona para pagina home
     {path: '/image/create', name:'create_image', component: RegisterImage, title: 'Cadastro', menu: true}, // direciona para pagina de cadastro das fotos
+    {path: '/image/create/:id', name:'updated_image', component: RegisterImage, title: 'Cadastro', menu: false}, // direct to paginate updated image
+    {path: '*', component: Home, menu: false} // caso não exista a rota direciona para pagina home
 ]
